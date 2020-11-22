7991334571e46300545ee886648ae2ee2f830521
Ilya Beltyukov <36417797+kanopeld@users.noreply.github.com>
2020-11-23 00:19:56 +0300

    Update restic.sh

--- restic.sh
+++ restic.sh
@@ -1,2 +1,2 @@
 #!/bin/bash
-RESTIC_PASSWORD=$2 restic -r $1 --exclude="/proc/*" --exclude="/dev/*" --exclude="" --exclude="/mnt/*" --exclude="/sys/*" --exclude="/tmp/*" --exclude="/run/*" --exclude="/media/*" --exclude="swapfile" --exclude="lost+found" --exclude=".cache" --exclude="Downloads" --exclude=".VirtualBoxVMs" --verbose backup /
+sudo RESTIC_PASSWORD=$2 restic -r $1 --exclude="/proc/*" --exclude="/dev/*" --exclude="" --exclude="/mnt/*" --exclude="/sys/*" --exclude="/tmp/*" --exclude="/run/*" --exclude="/media/*" --exclude="swapfile" --exclude="lost+found" --exclude=".cache" --exclude="Downloads" --exclude=".VirtualBoxVMs" --verbose backup /
